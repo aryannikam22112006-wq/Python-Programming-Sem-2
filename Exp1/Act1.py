7411d4fe87c1497dc12b5b4191742578cc00c7ca
aryannikam22112006-wq <aryannikam22112006@gmail.com>
2026-02-09 15:24:44 +0530

    Create Act1.py

---
+++ Exp1/Act1.py
@@ -0,0 +1,22 @@
+
+num1=int(input("Enter Number1:"))
+num2=int(input("Enter Number2:"))
+
+sum=num1+num2
+print("Addition=",sum)
+
+
+sub=num1-num2
+print("Subtraction=",sub)
+
+
+multi=num1*num2
+print("Multiplication=",multi)
+
+
+div=num1/num2
+print("Division=",div)
+
+
+mod=num1%num2
+print("Modulus=",mod)
