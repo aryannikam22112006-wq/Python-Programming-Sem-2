bfd50a7eddfec815eebbd706f92e30ce2954bf5b
aryannikam22112006-wq <aryannikam22112006@gmail.com>
2026-03-09 14:56:15 +0530

    Create EXP5.1.py

---
+++ EXP5/EXP5.1.py
@@ -0,0 +1,29 @@
+#  dictionary operations:add, delete, and update key-value pairs
+"""
+Created on Mon Mar  9 14:48:48 2026
+
+@author: Aryan
+"""
+
+# Creating an empty dictionary
+student = {}
+
+# Adding key-value pairs
+student["name"] = "Rahul"
+student["age"] = 20
+student["course"] = "Python"
+
+print("Dictionary after adding elements:")
+print(student)
+
+# Updating a value
+student["age"] = 21
+
+print("\nDictionary after updating a value:")
+print(student)
+
+# Deleting a key-value pair
+del student["course"]
+
+print("\nDictionary after deleting a key-value pair:")
+print(student)
