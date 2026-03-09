b2db6d7d7bb273ec22233b216a82a505b7091992
aryannikam22112006-wq <aryannikam22112006@gmail.com>
2026-03-09 14:43:57 +0530

    Create EXP4 act1.py

---
+++ EXP4/EXP4 act1.py
@@ -0,0 +1,16 @@
+#  find average and topper marks
+"""
+Created on Mon Mar  9 14:19:56 2026
+
+@author: Aryan
+"""
+
+marks =  [85, 56, 78, 56, 90 ]
+
+average_marks =  sum(marks) / len(marks)
+
+topper_marks = max(marks)
+
+print(f"Average marks: {average_marks:.2f}")
+
+print(f"Topper marks: {topper_marks}")
