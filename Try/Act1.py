ca1493e9d502606bc7a6a6151264466a3328b6c0
aryannikam22112006-wq <aryannikam22112006@gmail.com>
2026-03-16 14:36:14 +0530

    Create Act1.py

---
+++ Try/Act1.py
@@ -0,0 +1,13 @@
+# Program to print addition of two numbers
+
+"""
+Created on Mon Mar 16 14:31:16 2026
+
+@author: Aryan
+"""
+
+num1=int(input("Enter Number 1:"))
+num2=int(input("Enter Number 2:"))
+
+sum=num1+num2
+print("Addition=",sum)
