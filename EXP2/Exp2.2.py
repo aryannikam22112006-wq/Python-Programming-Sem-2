9806795782f52495203f948be828f012132a9e76
aryannikam22112006-wq <aryannikam22112006@gmail.com>
2026-03-16 14:45:04 +0530

    Add files via upload

---
+++ EXP2/Exp2.2.py
@@ -0,0 +1,13 @@
+# Find the factorial of a number using loops.
+"""
+Created on Mon Mar 16 14:42:35 2026
+
+@author: Aryan
+"""
+
+n = int(input("Enter number:"))
+
+fact = 1
+for i in range(1, n + 1):
+ fact = fact * i
+print("Factorial:", fact)
